0b2a0d1a5a605f593fa86417ca892aaa3aa5dc2c
jbavari <josh@drifty.com>
2015-11-12 17:25:43 -0700

    Fix(webpack): Remove semicolon, :derp:

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: [
     "reflect-metadata",
-    path.join(__dirname, 'www', 'app', 'app.js');
+    path.join(__dirname, 'www', 'app', 'app.js')
   ],
   output: {
     path: path.join(__dirname, 'www', 'build', 'js'),
