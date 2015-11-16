f20e37ca30ce4868c66900b78a53aa498e553a70
jbavari <josh@drifty.com>
2015-11-16 13:35:53 -0700

    Fix(webpack): Include autoprefixer in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         // can work with, e.g.: url('../my-file.png') => url('/path/to/my-file.png')
         // https://github.com/bholloway/resolve-url-loader
         test: /\.scss$/,
-        loaders: ["style", "css",  "resolve-url", "sass?sourceMap"]
+        loaders: ["style", "css", "autoprefixer?browsers=last 2 version", "resolve-url", "sass?sourceMap"]
       },
       // Any png-image or woff-font below or equal to 100K will be converted 
       // to inline base64 instead
