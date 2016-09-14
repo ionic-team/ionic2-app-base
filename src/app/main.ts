6b64e976a3966b8458c34cfc61c7225dbd63474c
Brandy Carney <brandyscarney@gmail.com>
2016-09-14 14:13:57 -0400

    chore: remove source file and unused dependencies

--- src/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppNgModuleNgFactory } from './ng-module.ngfactory';
-
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppNgModuleNgFactory);
